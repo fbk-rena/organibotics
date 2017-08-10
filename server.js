a3beade229963bf22dae5febc07ff7a886fac689
dianix <dianavazquez.dcg@gmail.com>
2017-08-09 22:51:00 -0500

    ajustes

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use('/static', express.static(path.join(__dirname,'node_modules')));
 app.use('/static', express.static(path.join(__dirname,'dist')));
 
 app.use('/',express.static("dist"));
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("listenning 3000");
