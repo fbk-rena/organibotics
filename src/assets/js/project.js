f06ff9f04daa1a45eab9d79624ee1b321e8d8e99
dianix <dianavazquez.dcg@gmail.com>
2017-08-10 14:27:50 -0500

    agregando funcionalidad a project

--- src/assets/js/project.js
+++ src/assets/js/project.js
@@ -1,4 +1,17 @@
-$(document).ready(function () {
+var pageLoaded = function () {
     $('.modal').modal();
     $('select').material_select();
-});
+    $("#addNewTask").submit(getTaskData);
+};
+
+var getTaskData = function () {
+    var taskName = $("#taskName").val();
+    var taskDescrip = $("#taskDescrip").val();
+    var assignedTime = $("#assignedTime").val();
+    var userSelected = $("#users").selectedIndex;
+    var roleSelected = $("#roles").selectedIndex;
+    console.log(taskName, taskDescrip, userSelected, roleSelected, assignedTime);
+}
+
+
+$(document).ready(pageLoaded);
